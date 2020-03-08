c7647dbeb2d38be34d5c5c4df0094512bdc815cd
Jakub Wrona <jakub.wrona97@gmail.com>
2020-03-08 22:05:19 +0100

    Folders and files from LAB1

---
+++ LB_Zaj1/DB.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlClient;
+using Microsoft.AspNet.SignalR.Infrastructure;
+
+
+namespace Zaj1
+{
+    public class DB
+    {
+        public void Select(SqlConnection connection)
+        {
+    
+            //open connection to execute code inside
+            connection.Open();
+
+            var query = "SELECT * FROM dbo.Customers";
+            var command = new SqlCommand(query, connection);
+            using var reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CompanyName"]);
+
+            }
+        }
+
+        public void Insert(SqlConnection connection, int id, string description)
+        {
+            var query = "INSERT INTO region(regionID, regionDescription)" + "VALUES(@id, @description)";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.AddWithValue("id", id);
+            command.Parameters.AddWithValue("description", description);
+            command.BeginExecuteNonQuery();
+            var affected = command.ExecuteNonQuery();
+            Console.WriteLine($"{affected} rows affected");
+
+
+        }
+        public void Delete(SqlConnection connection, int id, string description)
+             
+        {
+            var query = "DELETE FROM @table WHERE id=@id";
+            var command = new SqlCommand(query, connection);
+            command.Parameters.Add(table);
+            command.Parameters.Add(id);
+            var aff = command.ExecuteNonQuery();
+            Console.WriteLine($"removed {aff} row");
+            connection.Close();
+        }
+        //connection close to close query/code
+        
+
+        }
+
+
+
+}
+}
---
+++ LB_Zaj1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Zaj1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Connection to database
+            var connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            using var connection = new SqlConnection(connectionString);
+            var db = new DB();
+            connection.Open();
+
+            db.Select(connection);
+            db.Insert(connection, 15, "Bielsko");
+            db.Delete(connection, 15);
+
+            connection.Close();
+        
+        
+        
+        }
+    }
+}
