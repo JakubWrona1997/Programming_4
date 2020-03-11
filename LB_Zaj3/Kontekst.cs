5e93514f0e38672832e03191516f9dcced086c17
Jakub Wrona <jakub.wrona97@gmail.com>
2020-03-11 14:45:59 +0100

    Zajecia3

---
+++ LB_Zaj3/Kontekst.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+
+namespace Zaj3
+{
+    public class Kontekst : DbContext
+    {
+        public DbSet<Zajecia> Zajecia { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            //modelBuilder
+            //     .Entity<Zajecia>()
+            //     .Property(x => x.Name)
+            //     .HasMaxLength(255)
+            //     .IsRequired()
+            //     .HasColumnName("NazwaFluent");
+        }
+
+    }
+}
---
+++ LB_Zaj3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Zaj3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new Kontekst();
+            db.Database.EnsureCreated();
+            db.Zajecia.Add(new Zajecia()
+            { Id = 1, Name = "P4", Times = new DateTime(1, 1, 2020, 13, 30, 00) });
+            db.SaveChanges();
+            var zajecia = db.Zajecia.Where(x => x.Id > 2);
+
+            foreach (var item in zajecia)
+            {
+                Console.WriteLine( $"{item.Id}. {item.Name} {item.Times.ToShortTimeString()}");
+
+            }
+            var zajeciaDoZmiany = db.Zajecia.First(x => x.Name.StartsWith("P"));
+            zajeciaDoZmiany.Name = "AM2";
+            db.Update(zajeciaDoZmiany);
+            db.SaveChanges();
+        }
+    }
+}
