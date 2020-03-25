055efe68caf9abd9738910d24da4588347a0ccec
Jakub Wrona <jakub.wrona97@gmail.com>
2020-03-25 12:59:29 +0100

    LAB4

---
+++ LB_Zaj4/LB_Zaj4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using RestSharp;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace LB_Zaj4
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+         
+            var google = new RestClient("http://google.pl");
+            var ath = new RestClient("http://ath.bielsko.pl");
+            var wiki = new RestClient("http://pl.wikipedia.org");
+            var gov = new RestClient("http://gov.pl");
+            var request = new RestRequest("/");//http://google.pl
+            var tasks = new List<Task>();
+            stopwatch.Start();
+            tasks.Add(google.ExecuteAsync(new RestRequest("/"), Method.GET));
+            Console.WriteLine();
+            Console.WriteLine(stopwatch.Elapsed);
+            stopwatch.Stop();
+
+        }
+    }
+}
